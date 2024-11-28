3af91e1abbd54540ca6efa9a6b948813c50649d6
Jonathan PHILIPPE <jphilippe@fluence.eu>
2024-11-28 09:36:47 +0100

    Updates invitation message

--- app/texters/invitation_texter.rb
+++ app/texters/invitation_texter.rb
@@ -2,8 +2,7 @@ class InvitationTexter < ApplicationTexter
   def pending(invitation)
     @firstname = invitation.firstname
     content = "Cher #{@firstname},\n\n" \
-      "Nous avons bien reçu ta demande d'invitation à notre mariage et te remercions pour l'intérêt que tu portes à notre union.\n\n" \
-      "Nous te remercions pour ta demande.\n" \
+      "Nous avons bien reçu ton intention de présence à notre mariage et te remercions pour l'intérêt que tu portes à notre union.\n\n" \
       "Nous te recontacterons très prochainement pour te confirmer si nous pouvons t'ajouter à la liste des invités.\n\n" \
       "Avec toute notre affection,\n" \
       "Jonathan et Paul"
@@ -14,8 +13,8 @@ class InvitationTexter < ApplicationTexter
   def accepted(invitation)
     @firstname = invitation.firstname
     content = "Cher #{@firstname},\n\n" \
-      "Nous avons bien reçu ta demande d'invitation à notre mariage et te remercions pour l'intérêt que tu portes à notre union.\n\n" \
-      "C'est avec plaisir que nous t'invitons à notre mariage !\n" \
+      "Nous revenons à toi suite à ton intention de présence à notre mariage et te remercions encore une fois pour l'intérêt que tu portes a notre union.\n\n" \
+      "C'est avec plaisir que nous aimerions t'inviter à notre mariage !\n" \
       "Nous serions ravis de te compter parmi nos invités et de partager ce jour spécial avec toi.\n" \
       "Tu trouveras ci-joint l'invitation officielle avec tous les détails concernant la cérémonie et la réception.\n\n" \
       "Avec toute notre affection,\n" \
@@ -28,7 +27,7 @@ class InvitationTexter < ApplicationTexter
     @firstname = invitation.firstname
     @reason = reason
     content = "Cher #{@firstname},\n\n" \
-      "Nous avons bien reçu ta demande d'invitation à notre mariage et te remercions pour l'intérêt que tu portes à notre union.\n\n" \
+      "Nous revenons à toi suite à ton intention de présence à notre mariage et te remercions encore une fois pour l'intérêt que tu portes a notre union.\n\n" \
       "Nous te remercions sincèrement pour ta demande.\n" \
       "Malheureusement, #{@reason}.\n" \
       "Nous espérons que tu comprendras notre situation. Nous te souhaitons tout le meilleur.\n\n" \
